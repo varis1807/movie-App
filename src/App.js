351f202ce13b63bbfea3abdc02934f04937d43ae
varis <vrvarisrana123@gmail.com>
2021-07-18 20:12:26 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -4,32 +4,52 @@ import Category from "./Category";
 import Search from "./Search";
 import Table from "./Table";
 
-function App(props) {
-  return (
-    <React.Fragment>
-      <Navbar />
+class App extends React.Component {
+  state = {
+    noOfMovies: 0,
+    searchString: "",
+  };
 
-      <div className="row">
-        <div className="col-2 p-4">
-          <Category />
-        </div>
+  receiveMovieData = (number) => {
+    this.setState({ noOfMovies: number });
+  };
 
-        <div className="col-10 p-4">
-          <div className="row">
-            <div className="col-3">
-              <Search />
-            </div>
+  receiveSearchParam = (param) => {
+    this.setState({ searchString: param });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+
+        <div className="row">
+          <div className="col-2 p-4">
+            <Category />
           </div>
 
-          <div className="row">
-            <div className="col-8">
-              <Table />
+          <div className="col-10 p-4">
+            <div className="row">
+              <div className="col-3">
+                <Search
+                  noOfMovies={this.state.noOfMovies}
+                  receiveSearchParam={this.receiveSearchParam}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-8">
+                <Table
+                  sendData={this.receiveMovieData}
+                  searchString={this.state.searchString}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/Table.jsx
+++ src/Table.jsx
@@ -2,40 +2,52 @@ import React from "react";
 
 class Table extends React.Component {
   state = {
-    allMovies: [
-      {
-        title: "Terminator",
-        genre: "Action",
-        stock: "2",
-        rate: "2.5",
-      },
-      {
-        title: "Die Hard",
-        genre: "Action",
-        stock: "3",
-        rate: "2.5",
-      },
-      {
-        title: "Get Out",
-        genre: "Thriller",
-        stock: "1",
-        rate: "3.5",
-      },
-      {
-        title: "Trip to Italy",
-        genre: "Comedy",
-        stock: "5",
-        rate: "1.5",
-      },
-    ],
+    allMovies: [],
+    currPage: 1,
   };
 
+  componentDidMount() {
+    fetch("/movies")
+      .then(function (res) {
+        return res.json();
+      })
+      .then((json) => {
+        this.setState({ allMovies: json });
+
+        this.props.sendData(json.length);
+      });
+  }
+
   render() {
-    let numberOfPages = Math.ceil(this.state.allMovies.length / 5);
+    let moviesToDisplay = [];
+
+    if (this.props.searchString) {
+      let strToCompare = this.props.searchString.toLowerCase();
+
+      let moviesInState = this.state.allMovies;
+
+      for (let i = 0; i < moviesInState.length; i++) {
+        if (moviesInState[i].title.toLowerCase().includes(strToCompare)) {
+          moviesToDisplay.push(moviesInState[i]);
+        }
+      }
+    } else {
+      moviesToDisplay = this.state.allMovies;
+    }
+
+    let numberOfPages = Math.ceil(moviesToDisplay.length / 5);
     let arr = [];
     for (let i = 1; i <= numberOfPages; i++) {
       arr.push(i);
     }
+
+    let starting = (this.state.currPage - 1) * 5;
+    let ending = this.state.currPage * 5 - 1;
+
+    moviesToDisplay = moviesToDisplay.slice(
+      starting,
+      Math.min(ending, moviesToDisplay.length - 1) + 1
+    );
 
     return (
       <div>
@@ -51,16 +63,44 @@ class Table extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.allMovies.map((el) => {
+            {moviesToDisplay.map((el) => {
               return (
-                <tr>
+                <tr key={el._id}>
                   <td>{el.title}</td>
-                  <td>{el.genre}</td>
-                  <td>{el.stock}</td>
-                  <td>{el.rate}</td>
-                  <td>Like</td>
+                  <td>{el.genre.name}</td>
+                  <td>{el.numberInStock}</td>
+                  <td>{el.dailyRentalRate}</td>
+                  <td
+                    onClick={() => {
+                      let allMovies = this.state.allMovies;
+
+                      let index = allMovies.findIndex((e) => e._id == el._id);
+
+                      allMovies[index].liked
+                        ? (allMovies[index].liked = false)
+                        : (allMovies[index].liked = true);
+
+                      this.setState({ allMovies: allMovies });
+                    }}
+                  >
+                    {el.liked ? "Liked!" : "Like"}
+                  </td>
                   <td>
-                    <button type="button" class="btn btn-danger">
+                    <button
+                      type="button"
+                      class="btn btn-danger"
+                      onClick={() => {
+                        let allMovies = this.state.allMovies;
+
+                        allMovies = allMovies.filter((eli) => {
+                          return eli._id != el._id;
+                        });
+
+                        this.props.sendData(allMovies.length);
+
+                        this.setState({ allMovies: allMovies });
+                      }}
+                    >
                       Delete
                     </button>
                   </td>
@@ -72,15 +112,28 @@ class Table extends React.Component {
 
         <nav>
           <ul class="pagination">
-            <li class="page-item">
+            <li
+              class="page-item"
+              onClick={() => {
+                let currPage = this.state.currPage;
+                currPage--;
+                if (currPage < 1) currPage = 1;
+                this.setState({ currPage: currPage });
+              }}
+            >
               <a class="page-link" href="#">
                 Previous
               </a>
             </li>
 
-            {arr.map(function (el) {
+            {arr.map((el) => {
               return (
-                <li class="page-item">
+                <li
+                  class="page-item"
+                  onClick={() => {
+                    this.setState({ currPage: el });
+                  }}
+                >
                   <a class="page-link" href="#">
                     {el}
                   </a>
@@ -88,7 +141,15 @@ class Table extends React.Component {
               );
             })}
 
-            <li class="page-item">
+            <li
+              class="page-item"
+              onClick={() => {
+                let currPage = this.state.currPage;
+                currPage++;
+                if (currPage > numberOfPages) currPage = numberOfPages;
+                this.setState({ currPage: currPage });
+              }}
+            >
               <a class="page-link" href="#">
                 Next
               </a>
